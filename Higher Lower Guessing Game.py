11b9263457e50704e9d0228890ce7ab2b71f4b21
Althran <overlord106@gmail.com>
2022-06-02 20:34:21 +0300

    create a new tranning program

---
+++ Higher Lower Guessing Game.py
@@ -0,0 +1,23 @@
+import random
+
+game = 'y'
+numbers_attempt = 0
+win = 0
+print('Hello, this is Higher Lower Guessing Game')
+conceived_number = [i for i in range(0 ,101)]
+while game == 'y':
+    computer = random.choice(conceived_number)
+
+    while win == 0:
+        print('Print your number')
+        player = int(input())
+        if player == computer:
+            print('You win!, your numbers of attempts', numbers_attempt)
+            win = 1
+        elif player > computer:
+            print('Your answer is higher than conceived number, try again')
+        else:
+            print('Your answer is lower than conceived number, try again')
+        numbers_attempt += 1
+    print('Another game?) y/n')
+    game = input()
